d206c9e1a01f2df05c5254ea8c3e514f035222c9
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-06-01 17:40:02 +0200

    Add code to check answer and alert the user if they are correct or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let button of buttons) {
         button.addEventListener('click', function () {
             if (this.getAttribute('data-type') === 'submit') {
-                alert('You clicked Submit!')
+                checkAnswer()
             } else {
                 let gameType = this.getAttribute('data-type')
                 runGame(gameType)
@@ -36,7 +36,24 @@ function runGame(gameType) {
     }
 }
 
-function checkAnswer() {}
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value)
+    let calculatedAnswer = calculateCorrectAnswer()
+    let isCorrect = userAnswer === calculatedAnswer[0]
+
+    if (isCorrect) {
+        alert('Hey! You got it right!')
+    } else {
+        alert(`Your answer of ${userAnswer} is incorrect. The correct answer is ${calculatedAnswer[0]}`)
+    }
+
+    runGame(calculatedAnswer[1])
+}
 
 /**
  *Gets the operands (the numbers) and the operator (plus, minus etc)
