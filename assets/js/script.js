dbc7007526b51746f4fad4df037af50d07207105
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-06-06 13:40:19 +0200

    Finished javascript aspect of the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,7 +134,7 @@ function displayMultiplicationQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2
+    document.getElementById('operand1').textContent = operand1 * operand2
     document.getElementById('operand2').textContent = operand2 > operand2 ? operand2 : operand1
     document.getElementById('operator').textContent = '/'
 }
